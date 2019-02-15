5a98539d410517822ad0195b54a3ec47e999bbae
chris <christopherscaudill@gmail.com>
2019-02-14 20:14:58 -0500

    ic - just for kellen :)

--- PDGwinform/Form1.Designer.cs
+++ PDGwinform/Form1.Designer.cs
@@ -28,13 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3HS = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4NbrGa = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5Croll = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6CrSr = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7Rolls = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8LastS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pig Dice Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HighScore:";
+            // 
+            // label3HS
+            // 
+            this.label3HS.AutoSize = true;
+            this.label3HS.Location = new System.Drawing.Point(93, 34);
+            this.label3HS.Name = "label3HS";
+            this.label3HS.Size = new System.Drawing.Size(10, 13);
+            this.label3HS.TabIndex = 2;
+            this.label3HS.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "# of games:";
+            // 
+            // label4NbrGa
+            // 
+            this.label4NbrGa.AutoSize = true;
+            this.label4NbrGa.Location = new System.Drawing.Point(93, 64);
+            this.label4NbrGa.Name = "label4NbrGa";
+            this.label4NbrGa.Size = new System.Drawing.Size(10, 13);
+            this.label4NbrGa.TabIndex = 4;
+            this.label4NbrGa.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Current Roll:";
+            // 
+            // label5Croll
+            // 
+            this.label5Croll.AutoSize = true;
+            this.label5Croll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5Croll.Location = new System.Drawing.Point(57, 156);
+            this.label5Croll.Name = "label5Croll";
+            this.label5Croll.Size = new System.Drawing.Size(12, 16);
+            this.label5Croll.TabIndex = 6;
+            this.label5Croll.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Current Score:";
+            // 
+            // label6CrSr
+            // 
+            this.label6CrSr.AutoSize = true;
+            this.label6CrSr.Location = new System.Drawing.Point(59, 204);
+            this.label6CrSr.Name = "label6CrSr";
+            this.label6CrSr.Size = new System.Drawing.Size(10, 13);
+            this.label6CrSr.TabIndex = 9;
+            this.label6CrSr.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "# of Rolls:";
+            // 
+            // label7Rolls
+            // 
+            this.label7Rolls.AutoSize = true;
+            this.label7Rolls.Location = new System.Drawing.Point(93, 114);
+            this.label7Rolls.Name = "label7Rolls";
+            this.label7Rolls.Size = new System.Drawing.Size(10, 13);
+            this.label7Rolls.TabIndex = 11;
+            this.label7Rolls.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 89);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Last Score:";
+            // 
+            // label8LastS
+            // 
+            this.label8LastS.AutoSize = true;
+            this.label8LastS.Location = new System.Drawing.Point(93, 89);
+            this.label8LastS.Name = "label8LastS";
+            this.label8LastS.Size = new System.Drawing.Size(10, 13);
+            this.label8LastS.TabIndex = 13;
+            this.label8LastS.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(272, 226);
+            this.Controls.Add(this.label8LastS);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label7Rolls);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label6CrSr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5Croll);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label4NbrGa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label3HS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pig Dice Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3HS;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4NbrGa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5Croll;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6CrSr;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7Rolls;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8LastS;
     }
 }
 
--- PDGwinform/Form1.cs
+++ PDGwinform/Form1.cs
@@ -16,5 +16,68 @@ namespace PDGwinform
         {
             InitializeComponent();
         }
+
+        Random rng = new Random();
+        int NbGm = 0;
+        int HiSr = 0;
+        int total = 0;
+        int CuSc = 0;
+        int rolls = 0;
+        int lasts = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Roll();
+        }
+
+        private void Roll()
+        {
+            total = rng.Next(1, 7);
+            if (total != 1)
+            {
+                CuSc += total;
+                label5Croll.Text = total.ToString();
+                label6CrSr.Text = CuSc.ToString();
+                rolls++;
+                label7Rolls.Text = rolls.ToString();
+                return;
+            }
+            else
+            {
+                label5Croll.Text = total.ToString();
+                WinCheck();
+                return;
+            }
+        }
+
+        private void WinCheck()
+        {
+            total = 0;
+            rolls = 0;
+            lasts = CuSc;
+            label8LastS.Text = lasts.ToString();
+
+            if (CuSc > HiSr)
+            {
+                HiSr = CuSc;
+                NbGm++;
+                CuSc = 0;
+                label3HS.Text = HiSr.ToString();
+                label4NbrGa.Text = NbGm.ToString();
+                label6CrSr.Text = "";
+                //label5Croll.Text = "";
+                return;
+            }
+            else
+            {
+                NbGm++;
+                CuSc = 0;
+                label4NbrGa.Text = NbGm.ToString();
+                label6CrSr.Text = "";
+                //label5Croll.Text = "";
+                return;
+            }
+        }
     }
 }
